637ff7805893e20d566df1a8772db597bee54092
Evgenia <101995565+krokodila888@users.noreply.github.com>
2023-01-14 21:54:15 +0300

    Add files via upload

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -8,10 +8,13 @@ function MoviesCardList(props) {
   const moviesToRender1 = moviesToRender.slice([0], [(indexToRenderFirst + bonus)]);
 
   function addBonusToRender() {
-    console.log(window.innerWidth);
-    if ((window.innerWidth > 1124)) setBonus(bonus + 3);
-    if ((window.innerWidth <= 1124) && (window.innerWidth >= 768)) setBonus(bonus + 2);
+    console.log(indexToRenderFirst % 2);    
+    console.log(indexToRenderFirst % 3);
+    if ((window.innerWidth > 1124) && ((moviesToRender1.length % 3) === 0)) setBonus(bonus + 3);
+    if ((window.innerWidth <= 1124) && (window.innerWidth >= 768) && ((moviesToRender1.length % 2) === 0)) setBonus(bonus + 2);
     if ((window.innerWidth < 768)) setBonus(bonus + 1);
+    if ((window.innerWidth <= 1124) && ((moviesToRender1.length % 2) !== 0)) setBonus(bonus + 1);
+    if ((window.innerWidth > 1124) && ((moviesToRender1.length % 3) !== 0)) setBonus(bonus + (3 - (moviesToRender1.length % 3)));
   }
 
   return (
