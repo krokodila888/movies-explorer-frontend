fc8ae688a5613f29586f0f65474791e22b5a6e2b
Evgenia <101995565+krokodila888@users.noreply.github.com>
2023-01-16 20:50:02 +0300

    Delete src directory

--- src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ loggedIn, children  }) => {
-  return loggedIn ? children : <Navigate to="/" />;
-}
-
-export default ProtectedRoute;
